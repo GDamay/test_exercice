25cd3bff79225285839305d9f6cf875f0c46e63d
Gabriel Damay <gabriel.damay@telecom-paris.fr>
2023-06-20 17:42:33 +0200

    Really solve spaces and tabs problem

--- exercise1.py
+++ exercise1.py
@@ -1,16 +1,16 @@
 def fibonacci(n: int) -> int:
     """Compute the fibonacci sequence of index n
 
-	Parameters
-	----------
-	n : int
-		index of the fibonacci number to compute
+    Parameters
+    ----------
+    n : int
+        index of the fibonacci number to compute
 
-	Returns
-	-------
-	int
-		the n th number of the fibonacci sequence
-	"""
+    Returns
+    -------
+    int
+        the n th number of the fibonacci sequence
+    """
 
-	# TODO : Write here your solution to the first exercise
-	raise NotImplementedError("Exercise 1 has not been solved yet")
+    # TODO : Write here your solution to the first exercise
+    raise NotImplementedError("Exercise 1 has not been solved yet")
